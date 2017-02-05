bab6144f426aa7c363d9e46dcb2d420fbf74f603
switch-12345 <1501007@st.adojuku.ac.jp>
2017-02-05 16:30:46 +0900

    課題提出

--- app/src/main/java/jp/ac/asojuku/st/kadai001/OnClickActivity.java
+++ app/src/main/java/jp/ac/asojuku/st/kadai001/OnClickActivity.java
@@ -1,10 +1,13 @@
 package jp.ac.asojuku.st.kadai001;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import static jp.ac.asojuku.st.kadai001.R.string.usersOption;
 
 public class OnClickActivity extends AppCompatActivity
 implements View.OnClickListener{
@@ -24,20 +27,24 @@ implements View.OnClickListener{
         // 前の画面からのインテントを取得
         Intent preIntent = getIntent();
         // 選択肢を表すキーワードを文字列定義リソースから取得
-        String keyword = getResources().getString(R.string.usersOption);
+        String keyword = getResources().getString(usersOption);
         // 選択された文字列をキーワードを元に取得
-
+        String usersOption = (String)preIntent.getExtras().get(keyword);
         // 正解を文字列定義リソースから取得
         String correctWord = getResources().getString(R.string.usagi);
 
         // テキストビューの表示はデフォルトは不正解にしてあるので、正解時に表示を変更
-
+        if( usersOption.equals(correctWord) ){
+            TextView tv = (TextView)this.findViewById(R.id.tvAnsMsg);
+            tv.setText(R.string.seikaiMsg);
+        }
 
     }
 
     // 戻るボタンの動作
     @Override
     public void onClick(View v) {
-
+        Intent intent = new Intent(OnClickActivity.this,MainActivity.class);
+        startActivity(intent);
     }
 }
